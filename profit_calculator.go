1ac35a08e7442f74d82b61833943af83ea433510
abrahamkifleT <abrahamkifle1212@gmail.com>
2024-10-30 10:23:32 +0300

    exercise validate user input by me

--- profit_calculator.go
+++ profit_calculator.go
@@ -6,11 +6,22 @@ import (
 
 func main() {
 
-	revenue := getUserInput("Revenue: ")
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil || revenue <= 0 {
+		fmt.Println("Invalid input provided")
+		return
+	}
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil || expenses <= 0 {
+		fmt.Println("Invalid input provided")
+		return
+	}
 
-	expenses := getUserInput("Expenses: ")
-
-	taxRate := getUserInput("Tax Rate: ")
+	taxRate, err := getUserInput("Tax Rate: ")
+	if err != nil || taxRate <= 0 {
+		fmt.Println("Invalid input provided")
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
@@ -20,11 +31,15 @@ func main() {
 
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
-	fmt.Scan(&userInput)
-	return userInput
+	_, err := fmt.Scan(&userInput)
+	if err != nil {
+		return 0, err
+	}
+
+	return userInput, nil
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
